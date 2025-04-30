9309a90c09034ca63f1ec10c94650f89d85a789f
Pawbies <alex.fuchs970@gmail.com>
2025-04-30 15:08:44 +0200

    authorization methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+    def require_geraetewart!
+      unless authenticated? && Current.user.rank.in?([ "chef", "geraetewart" ])
+        redirect_to root_path, alert: "Nicht authorisiert"
+      end
+    end
+
+    def require_chef!
+      unless authenticated? && Current.user.rank == "chef"
+        redirect_to root_path, alert: "Nicht authorisiert"
+      end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_chef!, only: %i[ new create ]
   before_action :set_user, only: %i[ show ]
 
   def index
